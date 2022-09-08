7c0f33c9b6dd640aed52d3a1cd855e4409f48f5e
Simple <15623140205@163.com>
2022-09-08 16:19:27 +0800

    🐛修复: 修改培宝配置文件格式

--- build/gen-types.ts
+++ build/gen-types.ts
@@ -1,19 +1,18 @@
-import { outDir, projectRoot, wpRoot } from "./utils/paths";
-import glob from "fast-glob";
-import { Project, ModuleKind, ScriptTarget, SourceFile } from "ts-morph";
-import path from "path";
-import fs from "fs/promises";
-import { parallel, series } from "gulp";
-import { run, withTaskName } from "./utils";
-import { buildConfig } from "./utils/config";
-
+import { outDir, projectRoot, wpRoot } from './utils/paths'
+import glob from 'fast-glob'
+import { Project, ModuleKind, ScriptTarget, SourceFile } from 'ts-morph'
+import path from 'path'
+import fs from 'fs/promises'
+import { parallel, series } from 'gulp'
+import { run, withTaskName } from './utils'
+import { buildConfig } from './utils/config'
 
 export const genEntryTypes = async () => {
-  const files = await glob("*.ts", {
+  const files = await glob('*.ts', {
     cwd: wpRoot,
     absolute: true,
-    onlyFiles: true,
-  });
+    onlyFiles: true
+  })
   const project = new Project({
     compilerOptions: {
       declaration: true,
@@ -21,51 +20,42 @@ export const genEntryTypes = async () => {
       allowJs: true,
       emitDeclarationOnly: true,
       noEmitOnError: false,
-      outDir: path.resolve(outDir, "entry/types"),
+      outDir: path.resolve(outDir, 'entry/types'),
       target: ScriptTarget.ESNext,
       rootDir: wpRoot,
-      strict: false,
+      strict: false
     },
     skipFileDependencyResolution: true,
-    tsConfigFilePath: path.resolve(projectRoot, "tsconfig.json"),
-    skipAddingFilesFromTsConfig: true,
-  });
-  const sourceFiles: SourceFile[] = [];
+    tsConfigFilePath: path.resolve(projectRoot, 'tsconfig.json'),
+    skipAddingFilesFromTsConfig: true
+  })
+  const sourceFiles: SourceFile[] = []
   files.map((f) => {
-    const sourceFile = project.addSourceFileAtPath(f);
-    sourceFiles.push(sourceFile);
-  });
+    const sourceFile = project.addSourceFileAtPath(f)
+    sourceFiles.push(sourceFile)
+  })
   await project.emit({
-    emitOnlyDtsFiles: true,
-  });
+    emitOnlyDtsFiles: true
+  })
   const tasks = sourceFiles.map(async (sourceFile) => {
-    const emitOutput = sourceFile.getEmitOutput();
+    const emitOutput = sourceFile.getEmitOutput()
     for (const outputFile of emitOutput.getOutputFiles()) {
-      const filepath = outputFile.getFilePath();
-      await fs.mkdir(path.dirname(filepath), { recursive: true });
-      await fs.writeFile(
-        filepath,
-        outputFile.getText().replaceAll("@w-plus", "."),
-        "utf8"
-      );
+      const filepath = outputFile.getFilePath()
+      await fs.mkdir(path.dirname(filepath), { recursive: true })
+      await fs.writeFile(filepath, outputFile.getText().replaceAll('@w-plus', '.'), 'utf8')
     }
-  });
-  await Promise.all(tasks);
-};
+  })
+  await Promise.all(tasks)
+}
 export const copyEntryTypes = () => {
-  const src = path.resolve(outDir, "entry/types");
+  const src = path.resolve(outDir, 'entry/types')
   const copy = (module) =>
     parallel(
       withTaskName(`copyEntryTypes:${module}`, () =>
-        run(
-          `cp -r ${src}/* ${path.resolve(
-            outDir,
-            buildConfig[module].output.path
-          )}/`
-        )
+        run(`cp -r ${src}/* ${path.resolve(outDir, buildConfig[module].output.path)}/`)
       )
-    );
-  return parallel(copy("esm"), copy("cjs"));
-};
+    )
+  return parallel(copy('esm'), copy('cjs'))
+}
 
-export const genTypes = series(genEntryTypes, copyEntryTypes());
+export const genTypes = series(genEntryTypes, copyEntryTypes())
